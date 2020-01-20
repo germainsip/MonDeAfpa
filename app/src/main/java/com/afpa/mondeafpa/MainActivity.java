95bc34330b73db754e0234536c827d0c0990d87f
Germain SIPIERE <gsipiere@gmail.com>
2020-01-20 09:53:32 +0100

    fonctionnel

--- app/src/main/java/com/afpa/mondeafpa/MainActivity.java
+++ app/src/main/java/com/afpa/mondeafpa/MainActivity.java
@@ -1,14 +1,18 @@
 package com.afpa.mondeafpa;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static java.lang.Integer.valueOf;
 
 public class MainActivity extends AppCompatActivity {
     // déclaration des éléments du layout
     private Button button;
-
+    public static final String EXTRA_NBFACES = "com.afpa.myapplication.NBFACES";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void lancer(View view){
+        Intent intent = new Intent(this, ResultatActivity.class);
+        EditText nbFacesChoix = (EditText) findViewById(R.id.nbFaces);
+        int nbFaces = (int) Integer.parseInt(nbFacesChoix.getText().toString());
 
+        intent.putExtra(EXTRA_NBFACES,nbFaces);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/afpa/mondeafpa/ResultatActivity.java
+++ app/src/main/java/com/afpa/mondeafpa/ResultatActivity.java
@@ -1,13 +1,46 @@
 package com.afpa.mondeafpa;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class ResultatActivity extends AppCompatActivity {
+
+    private TextView resultat;
+    private int nbFaces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat);
+        // Récupère les infos de Intent
+        Intent intent = getIntent();
+        nbFaces = intent.getIntExtra(MainActivity.EXTRA_NBFACES,1);
+
+        String nbFacesSt ="Pour un Dé à "+ nbFaces +" Faces";
+        //String tirage = String.valueOf((int) (nbFaces * Math.random()));
+        // Trouve le TextView et modifie le texte.TextVia
+        resultat = findViewById(R.id.resultat);
+        TextView report = findViewById(R.id.nombreDeFacesReport);
+        report.setText(nbFacesSt);
+        relancer();
+    }
+
+
+
+
+    public void relancer(){
+        Random fortune = new Random();
+        int tirageNum = fortune.nextInt(nbFaces)+1;
+        this.resultat.setText(String.valueOf(tirageNum));
+
+    }
+
+    public void relancer(View view) {
+        relancer();
     }
 }
